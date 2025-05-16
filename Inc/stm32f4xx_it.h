c38d334976fe881ca8655b74461cb565ad59ce8b
moriirn <moriya8229@gmail.com>
2025-05-16 11:20:20 +0900

    fix: add uart4 interrupt

--- Inc/stm32f4xx_it.h
+++ Inc/stm32f4xx_it.h
@@ -59,6 +59,7 @@ void DMA1_Stream4_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void USART1_IRQHandler(void);
 void TIM8_TRG_COM_TIM14_IRQHandler(void);
+void UART4_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
